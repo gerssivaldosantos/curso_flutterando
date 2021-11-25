6cfa1ceedc3a31270598926321139d21edf9fa0e
gerssivaldosantos <canalandows@gmail.com>
2021-11-25 13:54:08 -0300

    inti

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const Center(
+    child: Text(
+      "OLá",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        fontSize: 40.0,
+        color: Colors.blueAccent,
+      ),
+    ),
+  ));
+}
