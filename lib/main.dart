0b8ec9549fb5b330ae6d4768ce7dec9c52a5c240
gerssivaldosantos <canalandows@gmail.com>
2021-11-25 14:26:55 -0300

    a simple widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(const Center(
-    child: Text(
-      "OLá",
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-        fontSize: 40.0,
-        color: Colors.blueAccent,
-      ),
-    ),
-  ));
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return (const Center(
+      child: Text("Curso Flutter #6",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(fontSize: 50.0, color: Colors.white60)),
+    ));
+  }
 }
