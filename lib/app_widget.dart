c678adf6fe1c833c7fba12692399c6f77901a7ec
gerssivaldosantos <canalandows@gmail.com>
2021-12-30 15:27:26 -0300

    manual routes

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,24 +1,15 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, unused_import, prefer_const_constructors
 
-import 'package:curso_flutterando/app_controller.dart';
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'login_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-        animation: AppController.instance,
-        builder: (context, child) {
-          return MaterialApp(
-            home: HomePage(),
-            theme: ThemeData(
-                primarySwatch: Colors.red,
-                brightness: AppController.instance.isDarkTheme
-                    ? Brightness.dark
-                    : Brightness.light),
-            debugShowCheckedModeBanner: false,
-          );
-        });
+    return MaterialApp(
+      home: LoginPage(),
+      theme: ThemeData(primarySwatch: Colors.red, brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:curso_flutterando/initial_page.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -13,53 +14,62 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
+  auth() {
+    if (password == 'teste' && email == 'canalandows') {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => InitialPage()));
+    } else {
+      debugPrint('Não autenticado ! ');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(15, 0, 15, 12),
-              child: TextField(
-                onChanged: (text) {
-                  email = text;
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 0, 15, 12),
+                child: TextField(
+                  onChanged: (text) {
+                    email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(15, 0, 15, 12),
-              child: TextField(
-                onChanged: (text) {
-                  password = text;
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Senha',
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 0, 15, 12),
+                child: TextField(
+                  onChanged: (text) {
+                    password = text;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Senha',
+                  ),
                 ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                debugPrint(
-                    'Os valores Obtidos são\nemail:$email\npassword:$password');
-              },
-              child: Text('Entrar'),
-            )
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  auth();
+                },
+                child: Text('Entrar'),
+              )
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
