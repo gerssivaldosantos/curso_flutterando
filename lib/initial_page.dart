8f586bbbd163ce5e859ef8cafaf076067c3d630d
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-01-14 20:44:09 -0300

    navigation and button

--- lib/initial_page.dart
+++ lib/initial_page.dart
@@ -12,6 +12,10 @@ class _InitialPageState extends State<InitialPage> {
     Navigator.of(context).pushReplacementNamed('/');
   }
 
+  goToCounterPage() {
+    Navigator.of(context).pushReplacementNamed('/counter_page');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,15 +26,32 @@ class _InitialPageState extends State<InitialPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Você se logou com sucesso ! '),
+            const Text(
+              'Lista de páginas',
+              style: TextStyle(fontSize: 30),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: goToCounterPage,
+                child: const Text(
+                  'Contador',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
             const SizedBox(
               height: 20,
             ),
             ElevatedButton(
-                onPressed: () {
-                  backToLogin();
-                },
-                child: const Text('Voltar'))
+              onPressed: () {
+                backToLogin();
+              },
+              child: const Text('Voltar'),
+            )
           ],
         ),
       ),
