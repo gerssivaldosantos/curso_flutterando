53954ce27f452df46cd419a8ea62b3c30aed657b
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-01-14 06:49:44 -0300

    network image, new counter buttons, solid and componetization

---
+++ lib/counter_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+  int _largestCounter = 0;
+
+  increment() {
+    setState(() {
+      _counter++;
+      if (_counter > _largestCounter) {
+        _largestCounter = _counter;
+      }
+    });
+  }
+
+  decrement() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  reset() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Spacer(),
+            Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Image.network(
+                    "http://www.retroplace.com/pics/c64/packshots/17060--tapper.png")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('High score $_largestCounter',
+                    style: Theme.of(context).textTheme.headline5),
+                SizedBox(width: 20),
+                Text(
+                  'Taps $_counter',
+                  style: TextStyle(fontSize: 40.0),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  child: Icon(Icons.add),
+                  onPressed: increment,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.remove),
+                  onPressed: decrement,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.autorenew),
+                  onPressed: reset,
+                ),
+              ],
+            ),
+            Spacer(
+              flex: 2,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
