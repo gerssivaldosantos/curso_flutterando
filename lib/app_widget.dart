2e3de6aaf33735759ebf276d7480148b077dcaa2
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-01-14 20:44:24 -0300

    light mode

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_key_in_widget_constructors, unused_import, prefer_const_constructors
 
+import 'counter_page.dart';
 import 'initial_page.dart';
 import 'package:flutter/material.dart';
 import 'login_page.dart';
@@ -8,12 +9,13 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red, brightness: Brightness.dark),
+      theme: ThemeData(primarySwatch: Colors.red, brightness: Brightness.light),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (context) => LoginPage(),
-        '/initial_page': (context) => InitialPage()
+        '/initial_page': (context) => InitialPage(),
+        '/counter_page': (context) => CounterPage(),
       },
     );
   }
