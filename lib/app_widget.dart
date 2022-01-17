ca21a0ea35ba49e57f14f03fdc8c40a33b4866c0
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-01-17 13:00:57 -0300

    organization

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: use_key_in_widget_constructors, unused_import, prefer_const_constructors
 
-import 'counter_page.dart';
-import 'initial_page.dart';
 import 'package:flutter/material.dart';
-import 'login_page.dart';
+import 'package:projeto/pages/counter_page.dart';
+import 'package:projeto/pages/initial_page.dart';
+import 'package:projeto/pages/login_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
@@ -11,7 +11,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.red, brightness: Brightness.light),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/initial_page',
       routes: {
         '/': (context) => LoginPage(),
         '/initial_page': (context) => InitialPage(),
