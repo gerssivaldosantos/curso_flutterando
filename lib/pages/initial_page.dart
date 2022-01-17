fbb33d7c1f13bdffc622bc9c2b4fb7432e84ce59
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-01-17 13:54:29 -0300

    stack

--- lib/pages/initial_page.dart
+++ lib/pages/initial_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projeto/widgets/blue_button.dart';
+import 'package:projeto/widgets/blue_button_outlined.dart';
 
 class InitialPage extends StatefulWidget {
   const InitialPage({Key? key}) : super(key: key);
@@ -17,6 +18,10 @@ class _InitialPageState extends State<InitialPage> {
     Navigator.of(context).pushReplacementNamed('/counter_page');
   }
 
+  goToUsersPage() {
+    Navigator.of(context).pushReplacementNamed('/users_page');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,24 +29,39 @@ class _InitialPageState extends State<InitialPage> {
         title: const Text('Bem-vindo'),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Lista de páginas',
-              style: TextStyle(fontSize: 30),
+      body: Stack(
+        children: [
+          Image.network(
+            'https://images.pexels.com/photos/4065158/pexels-photo-4065158.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            fit: BoxFit.cover,
+            height: MediaQuery.of(context).size.height,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Lista de páginas',
+                  style: TextStyle(fontSize: 25, color: Colors.white),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                blueButton(
+                    context, "Contador", 25.0, 50.0, 0.3, goToCounterPage),
+                const SizedBox(
+                  height: 20,
+                ),
+                blueButton(context, "Usuários", 25.0, 50.0, 0.3, goToUsersPage),
+                const SizedBox(
+                  height: 20,
+                ),
+                blueButtonOutlined(
+                    context, "Voltar ao Login", 25.0, 50.0, 0.3, backToLogin)
+              ],
             ),
-            SizedBox(
-              height: 20,
-            ),
-            blueButton(context, "Contador", 25.0, 50.0, 0.3, goToCounterPage),
-            const SizedBox(
-              height: 20,
-            ),
-            blueButton(context, "Voltar ao Login", 25.0, 50.0, 0.3, backToLogin)
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
